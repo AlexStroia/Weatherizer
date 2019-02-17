ac10bdce7fe1d741162476a2e3c29dcbac5b7bb5
Alex Stroia <salexandru995@gmail.com>
2019-02-17 08:36:34 -0800

    Refactored folders

--- Weatherizer/app/src/main/java/co/alexdev/weatherizer/ui/fragment/BaseFragment.java
+++
@@ -1,6 +0,0 @@
-package co.alexdev.weatherizer.ui.fragment;
-
-import androidx.fragment.app.Fragment;
-
-public class BaseFragment extends Fragment {
-}
--- Weatherizer/app/src/main/java/co/alexdev/weatherizer/utils/PrefManager.java
+++
@@ -1,4 +0,0 @@
-package co.alexdev.weatherizer.utils;
-
-public class PrefManager {
-}
